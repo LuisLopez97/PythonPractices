cd81eb3f3b6dd008cd5dd2365bac522052e8c11f
Luis Antonio Lopez Mendez <luislopezmendez97@gmail.com>
2019-03-12 18:07:59 -0600

    Operadores
    Primeras operaciones aritmeticas y de concatenacion en Python

---
+++ pythonCurso/operadores.py
@@ -0,0 +1,10 @@
+# Programa que realiza una sencilla operacion.
+operacion = 1-5*6+20
+operacion2 = 4
+print(operacion+operacion2)
+
+operacion3 = "2"
+operacion4 = "2"
+
+resultado = "2 mas 2 no es "+operacion3 + operacion4
+print(resultado)
