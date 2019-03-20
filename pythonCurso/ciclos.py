12c3f44e4cf88e217b7d471e88828c9812d9c9ce
Luis Antonio Lopez Mendez <luislopezmendez97@gmail.com>
2019-03-19 18:11:28 -0600

    Cuarto y Quinto programa
    Programas de condicionales y ciclos en Python

---
+++ pythonCurso/ciclos.py
@@ -0,0 +1,18 @@
+# ciclo while
+a = 0
+b = 100
+c = 0
+# while(a<b):
+#     c+=1
+    # print(c)
+
+# CICLO for value in variable:
+# for x in range(b):
+    # print(x)
+
+# CICLO "FOR EACH"
+lista = ["manzana","pera","platano"]
+for elemento in lista:
+    if(elemento!="manzana"):
+        print(elemento)
+print(len(lista))
---
+++ pythonCurso/condicionales.py
@@ -0,0 +1,8 @@
+a=int(input("Ingresa un numero: "))
+if(a>10):
+    print("a es mayor a 10")
+elif(a<0):
+    print("el numero es negativo")
+
+else:
+    print("el numero es menor a 10")
